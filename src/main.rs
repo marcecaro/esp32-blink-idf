083c0a27f35e7ff241baf983dbbaf604bf28a24b
mcaro <marcecaro@gmail.com>
2025-04-07 10:08:37 -0700

    Adding blink test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::thread;
 use std::time::Duration;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::peripherals::Peripherals;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -9,13 +11,25 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Starting loop with delays...");
+    log::info!("Starting LED blink loop...");
+    
+    // Get access to the ESP32 peripherals
+    let peripherals = Peripherals::take()?;
+    
+    // The built-in LED on most ESP32 dev boards is connected to GPIO2
+    let led_pin = peripherals.pins.gpio2;
+    
+    // Create a pin driver for the LED pin in output mode
+    let mut led = PinDriver::output(led_pin)?;
     
     // Create an infinite loop
     let mut counter = 0;
+    
     loop {
-        // Log message with counter
-        log::info!("Loop iteration: {}", counter);
+        // Toggle LED state
+
+        led.toggle()?;
+        log::info!("LED ON - Loop iteration: {}", counter);
         
         // Increment counter
         counter += 1;
@@ -23,4 +37,8 @@ fn main() {
         // Delay for 1 second
         thread::sleep(Duration::from_secs(1));
     }
+    
+    // This line is never reached due to the infinite loop
+    #[allow(unreachable_code)]
+    Ok(())
 }
